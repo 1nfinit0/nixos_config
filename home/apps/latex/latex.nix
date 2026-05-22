b5569c37de6e83ae06a9e39ea536b1ad5f916589
tobi <noggnzzz@gmail.com>
2026-05-22 00:03:24 -0500

    + latexpkgs

--- home/apps/latex/latex.nix
+++ home/apps/latex/latex.nix
@@ -4,56 +4,36 @@
   home.packages = with pkgs; [
     zathura
     texlab
-    tectonic
 
     (texlive.combine {
       inherit (texlive)
         scheme-medium
 
-        # Idioma / encoding
+        # Idioma
         babel-spanish
         collection-langspanish
 
         # Matemáticas
-        amsmath
-        amssymb
+        amsfonts
         cancel
         eulervm
 
         # Gráficos
         pgf
         pgfplots
-        tikzfill
-        graphicx
-        caption
-        subcaption
 
         # Estilo / formato
         fancyhdr
         titlesec
         titling
         tocloft
-        setspace
-        geometry
-
-        # Layout
-        array
-        multicol
-        float
-        indentfirst
 
         # Referencias
         hyperref
         tocbibind
 
-        # Fonts/encoding
-        fontenc
-        inputenc
-
-        # Utilidades comunes
-        latexmk
-        xcolor
-        tools;
+        # Utilidades
+        latexmk;
     })
   ];
 }
