ec519938882348f5578e1422d6f4ae6bc674a2f8
tobi <noggnzzz@gmail.com>
2026-05-22 00:32:43 -0500

    + latexpkgs

--- home/apps/latex/latex.nix
+++ home/apps/latex/latex.nix
@@ -1,39 +1,38 @@
 { pkgs, ... }:
-
 {
   home.packages = with pkgs; [
-    zathura
     texlab
-
     (texlive.combine {
       inherit (texlive)
         scheme-medium
-
         # Idioma
         babel-spanish
         collection-langspanish
-
         # Matemáticas
         amsfonts
         cancel
         eulervm
-
         # Gráficos
         pgf
         pgfplots
-
         # Estilo / formato
         fancyhdr
         titlesec
         titling
         tocloft
-
         # Referencias
         hyperref
         tocbibind
-
         # Utilidades
         latexmk;
     })
   ];
+
+  programs.zathura = {
+    enable = true;
+    options = {
+      synctex = true;
+      synctex-editor-command = "code --goto %{input}:%{line}";
+    };
+  };
 }
