4e0b97553ce664ad521192c24692376026fd15c2
tobi <noggnzzz@gmail.com>
2026-08-13 19:40:02 -0500

    cisco packet traCER

--- home/apps/cisco/cisco.nix
+++ home/apps/cisco/cisco.nix
@@ -1,8 +1,6 @@
 { pkgs, ... }:
 {
   home.packages = [
-    (pkgs.ciscoPacketTracer9.override {
-      packetTracerSource = ../../../assets/cisco-packet-tracer/CiscoPacketTracer_901_Ubuntu_64bit.deb;
-    })
+    pkgs.ciscoPacketTracer9
   ];
 }
